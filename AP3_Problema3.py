c711b2f0519b714de747f4e1f40954878e0eff21
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-18 17:40:53 -0300

    Create AP3_Problema3.py

---
+++ AP3_Problema3.py
@@ -0,0 +1,25 @@
+def verifica_triangulo(x, y, z):
+    # Verifica a desigualdade triangular
+    if x < y + z and y < x + z and z < x + y:
+        return True
+    else:
+        return False
+
+def tipo_triangulo(x, y, z):
+    if x == y == z:
+        return "Equilátero"
+    elif x == y or y == z or x == z:
+        return "Isósceles"
+    else:
+        return "Escaleno"
+
+# Exemplos de uso das funções
+lado1 = float(input("Digite o valor do primeiro lado: "))
+lado2 = float(input("Digite o valor do segundo lado: "))
+lado3 = float(input("Digite o valor do terceiro lado: "))
+
+if verifica_triangulo(lado1, lado2, lado3):
+    tipo = tipo_triangulo(lado1, lado2, lado3)
+    print(tipo)
+else:
+    print("Não forma triângulo")
