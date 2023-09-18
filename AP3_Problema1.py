ef354ec8d4e221fb7a90c2152fdb8f29d4a70c01
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-18 17:38:08 -0300

    Create AP3_Problema1.py

---
+++ AP3_Problema1.py
@@ -0,0 +1,18 @@
+def pagamento(salario_atual):
+    if salario_atual <= 280.00:
+        aumento = salario_atual * 0.20
+    elif salario_atual <= 700.00:
+        aumento = salario_atual * 0.15
+    elif salario_atual <= 1500.00:
+        aumento = salario_atual * 0.10
+    else:
+        aumento = salario_atual * 0.05
+
+    novo_salario = salario_atual + aumento
+    return novo_salario
+
+# Exemplos de uso da função
+salario = float(input("Digite o valor do salário: "))
+novo_salario = pagamento(salario)
+print(f"Valor do aumento: {novo_salario - salario:.2f}")
+print(f"Novo salário: {novo_salario:.2f}")
