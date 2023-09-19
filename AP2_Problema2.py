0ebf211d73ee141b034e7c26ba47ffd7505800c1
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-19 18:57:43 -0300

    Create AP2_Problema2.py

---
+++ AP2_Problema2.py
@@ -0,0 +1,17 @@
+vel_maxima = float(input("Digite o valor da velocidade máxima: "))
+vel_registrada = float(input("Digite o valor da velocidade registrada: "))
+
+del multa (vel_registrada, vel_maxima): 
+ 
+  if vel_registrada <= vel_maxima:
+       multa == "Sem infração"
+  elif vel_registrada > vel_maxima, vel_registrada <= (1.2 * vel_maxima)
+       multa == "Infração média"
+  elif vel_registrada > 1.2 * vel_maxima, vel_registrada < 1.5 * vel_maxima
+       multa == "Infração grave"
+  elif vel_registrada >= 1.5 * vel_maxima
+       multa == "Infração Gravíssima"
+
+return multa 
+
+print(f"Multa : {multa})
