1ffa685245482745ca4dbf0a999ed791ee8dec91
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-19 17:37:19 -0300

    Update AP3_Problema5.py

--- AP3_Problema5.py
+++ AP3_Problema5.py
@@ -1,9 +1,12 @@
-altura = float(input("Digite a aultura: "))
+
+altura = float(input("Digite a altura: "))
 sexo = str(input("Digite o sexo (F/M): "))
 
-peso_ideal = if(sexo = "F"):
-                (62.1 * altura) - 44.7  
-             elif(sexo = "M"):
-                (72.7 * altura) - 58
-print(f"Peso ideal: {peso_ideal}")
- 
+def peso_ideal(altura, sexo):
+    if sexo == "F":
+        peso_ideal = (62.1 * altura) - 44.7
+    elif sexo == "M":
+        peso_ideal = (72.7 * altura) - 58
+    else:
+        peso_ideal = None  # Sexo não reconhecido
+    return peso_ideal
