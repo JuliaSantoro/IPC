63ae557e4a0ddee0bc50aa76a6128306b38c3ae6
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-19 19:14:38 -0300

    Create AP2_Problema4.py

---
+++ AP2_Problema4.py
@@ -0,0 +1,16 @@
+salario1 = float(input("Digite o valor do salário: "))
+
+if salario1 <= 280.00
+    aumento = 0.20
+elif salari1 <= 700.00:
+    aumento = 0.15
+elif salario1 <= 1500.00:
+    aumento = 0.10
+else:
+    aumento = 0.05
+
+final = salario1 * aumento
+novo_salario = salario1 + final
+
+print(f"Valor do aumento: {aumento:.2f}")
+print(f"Novo salário: {novo_salario:.2f}")
