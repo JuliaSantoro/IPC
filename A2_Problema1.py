380f8496491c6a47954c05eb1992ae9fc8aeda28
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-19 19:43:15 -0300

    Update A2_Problema1.py

--- A2_Problema1.py
+++ A2_Problema1.py
@@ -1,13 +1,13 @@
 
-distancia_arremesso = float(input("Digite a distância do arremesso à cesta (em metros): "))
-
 pontuacao_total = 0
 
-    if distancia_arremesso > 7.24:
+for i in range(3):
+    distancia_arremesso = float(input(f"Digite o {i + 1}º arremesso (-1 para lance livre): "))
+    if distancia_arremesso == -1:
+        pontuacao_total += 1
+    elif distancia_arremesso > 7.24:
         pontuacao_total += 3
     elif distancia_arremesso <= 7.24:
         pontuacao_total += 2
-    else:
-        pontuacao_total += 1
 
-print(f"A pontuação total das três primeiras jogadas é: {pontuacao_total}")
+print(f"Pontuação: {pontuacao_total}")
