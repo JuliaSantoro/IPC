579a1c7483899255e7d06874b05dcf229c8bcf07
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-19 19:09:25 -0300

    Update AP2_Problema2.py

--- AP2_Problema2.py
+++ AP2_Problema2.py
@@ -1,12 +1,12 @@
 vel_maxima = float(input("Digite o valor da velocidade máxima: "))
 vel_registrada = float(input("Digite o valor da velocidade registrada: "))
-resultado = velocidade_registrada - velocidade_maxima
+resultado = vel_registrada - vel_maxima
 
 if resultado <= 0:
     print("Sem Infração")
-elif resultado <= 0.2 * velocidade_maxima:
+elif resultado <= 0.2 * vel_maxima:
     print("Infração Média")
-elif resultado <= 0.5 * velocidade_maxima:
+elif resultado <= 0.5 * vel_maxima:
     print("Infração Grave")
 else:
     print("Infração Gravíssima") 
