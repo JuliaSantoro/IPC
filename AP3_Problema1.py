1c2862f065871b404f691a0b1d61b1d80d8d7fb1
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-20 21:20:51 +0000

    mudancas

--- AP3_Problema1.py
+++ AP3_Problema1.py
@@ -1,14 +1,14 @@
 def pagamento(salario_atual):
     if salario_atual <= 280.00:
-        aumento = salario_atual * 0.20
+        aumento = 1.20
     elif salario_atual <= 700.00:
-        aumento = salario_atual * 0.15
+        aumento = 1.15
     elif salario_atual <= 1500.00:
-        aumento = salario_atual * 0.10
+        aumento = 1.10
     else:
-        aumento = salario_atual * 0.05
+        aumento = 1.05
 
-    novo_salario = salario_atual + aumento
+    novo_salario = salario_atual * aumento
     return novo_salario
 
 salario = float(input("Digite o valor do salário: "))
