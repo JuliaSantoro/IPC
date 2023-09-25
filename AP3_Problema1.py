0734bea0224391669515de668052184b49d2f180
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-25 16:20:55 -0300

    Update AP3_Problema1.py

--- AP3_Problema1.py
+++ AP3_Problema1.py
@@ -1,15 +1,14 @@
 def pagamento(salario_atual):
-    if salario_atual <= 280.00:
-        aumento = 0.20
-    elif salario_atual <= 700.00:
-        aumento = 0.15
-    elif salario_atual <= 1500.00:
-        aumento = 0.10
+    if salario_atual <= 280:
+        aumento = salario_atual * 0.20
+    elif salario_atual <= 700:
+        aumento = salario_atual * 0.15
+    elif salario_atual <= 1500:
+        aumento = salario_atual * 0.10
     else:
-        aumento = 0.05
+        aumento = salario_atual * 0.05
 
-    novo_salario = (salario_atual * aumento) + salario_atual
+    novo_salario = salario_atual + aumento
+
     return novo_salario
-
-
 
