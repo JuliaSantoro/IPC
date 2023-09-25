c371b47910e6f8325b68bf3700861e9be78985cc
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-25 16:29:48 -0300

    Update AP3_Problema4.py

--- AP3_Problema4.py
+++ AP3_Problema4.py
@@ -1,24 +1,16 @@
-litros = float(input("Digite a quantidade de litros: "))
-tipo = input("Digite o tipo de combustível (a para Álcool ou g para Gasolina): ")
-preco_litro = float(input("Digite o preço do litro de combustível: "))
-
-def calcula_valor (preco_litro, litros_abastecidos, tipo_combustivel):
+def calcula_valor(preco_litro, litros, tipo_combustivel):
     if tipo_combustivel == "a":
-        if litros_abastecidos <= 20:
-            desconto_por_litro = 0.03
+        if litros <= 20:
+            desconto = 0.03
         else:
-            desconto_por_litro = 0.05
+            desconto = 0.05
     elif tipo_combustivel == "g":
-        if litros_abastecidos <= 20:
-            desconto_por_litro = 0.04
+        if litros <= 20:
+            desconto = 0.04
         else:
-            desconto_por_litro = 0.06
+            desconto = 0.06
     else:
-        raise ValueError("Tipo de combustível inválido. Use 'a' para Álcool ou 'g' para Gasolina.")
+        raise ValueError("Tipo de combustível inválido. Use 'a' para álcool ou 'g' para gasolina.")
 
-    total = preco_litro * litros_abastecidos * (1 - desconto_por_litro)
-    return total
-
-
-total = calcula_valor(preco_litro, litros, tipo)
-print(f"Total: {total:.2f}")
+    valor_pago = preco_litro * litros * (1 - desconto)
+    return valor_pago
