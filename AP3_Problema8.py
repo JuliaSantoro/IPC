7ca7ec2a7f41948625c14a74e0d71867b62e4a3a
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-25 16:45:51 -0300

    Update AP3_Problema8.py

--- AP3_Problema8.py
+++ AP3_Problema8.py
@@ -1,19 +1,12 @@
-def estacionamento(hora_entrada, minuto_entrada, hora_saida, minuto_saida):
-    # Verifica se o estacionamento funciona entre 08:00 e 18:00
-    if hora_entrada < 8 or hora_entrada >= 18 or hora_saida < 8 or hora_saida >= 18:
-        return "Horário de funcionamento do estacionamento é entre 08:00 e 18:00"
-    
-    # Calcula o tempo de estacionamento em minutos
-    tempo_estacionado = (hora_saida * 60 + minuto_saida) - (hora_entrada * 60 + minuto_entrada)
-    
-    # Calcula o número de horas a pagar arredondando para cima
-    horas_a_pagar = (tempo_estacionado + 59) // 60
-    
-    # Calcula o valor total devido (R$1 por hora)
-    valor_total = horas_a_pagar
-    
-    return f"Preço: R$ {valor_total:.2f}"
+def estacionamento(entrada_hora, entrada_minuto, saida_hora, saida_minuto):
+    entrada_total_minutos = entrada_hora * 60 + entrada_minuto
+    saida_total_minutos = saida_hora * 60 + saida_minuto
 
-# Exemplos de uso da função
-print(estacionamento(8, 0, 8, 1))
-print(estacionamento(12, 30, 15, 31))
+    if saida_total_minutos < 480 or entrada_total_minutos > 1080:
+        return 0.00
+
+    horas_estacionadas = (saida_total_minutos - entrada_total_minutos + 59) // 60
+
+    preco = horas_estacionadas
+
+    return preco
