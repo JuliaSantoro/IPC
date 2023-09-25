99a107b3094ddc2d6c99011ec7cc1f205639acf8
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-25 16:25:20 -0300

    Update AP3_Problema3.py

--- AP3_Problema3.py
+++ AP3_Problema3.py
@@ -1,20 +1,14 @@
-x = float(input("Digite o valor do primeiro lado: "))
-y = float(input("Digite o valor do segundo lado: "))
-z = float(input("Digite o valor do terceiro lado: "))
-
-
-def eh_triangulo(x, y, z):
-    if x < y + z and y < x + z and z < x + y:
+def verifica_triangulo(lado1, lado2, lado3):
+    # Verifica a desigualdade triangular
+    if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2:
         return True
     else:
         return False
 
-    if eh_triangulo == true and x == y == z:
-        print("Equilátero")
-    elif eh_triangulo == true and (x == y or y == z or x == z):
-        print("Isósceles")
-    elif eh_triangulo == true and (x != y != z) 
-   print("Escaleno")
-
-else:
-    print("Não forma triângulo")
+def tipo_triangulo(lado1, lado2, lado3):
+    if lado1 == lado2 and lado2 == lado3:
+        return "Equilátero"
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        return "Isósceles"
+    else:
+        return "Escaleno"
