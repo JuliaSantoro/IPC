81d77a65bf4b35bfd5bb067320f2bf3e58d82162
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-27 16:09:11 -0300

    Update A3_Problema1.py

--- A3_Problema1.py
+++ A3_Problema1.py
@@ -1,17 +1,14 @@
 def progressoes(a1, r, n):
-    # Cálculo do enésimo termo da PA
+   
     an_pa = a1 + (n - 1) * r
 
-    # Cálculo do enésimo termo da PG
-    q = a1 * (an_pa / a1) ** (1 / (n - 1))
+    q = (an_pa / a1) ** (1 / (n - 1))
 
-    # Cálculo da soma dos n primeiros termos da PA
     sn_pa = n * (a1 + an_pa)
 
-    # Cálculo da soma dos n primeiros termos da PG
-    sn_pg = a1 * (q ** n - 1) / (q - 1)
+    sn_pg = a1 * ((q ** n) - 1) / (q - 1)
 
-    if sn_pa > sn_pg:
+    if an_pa > a1 * (q ** (n - 1)):
         return int(sn_pa)
     else:
         return int(sn_pg)
