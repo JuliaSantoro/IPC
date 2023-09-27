a6a783c0ffbfedc2c0355596592e7b8dc019d706
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-09-27 16:57:52 -0300

    Create A3_Problema3.py

---
+++ A3_Problema3.py
@@ -0,0 +1,12 @@
+def estacionamento(hora_chegada, minuto_chegada, hora_partida, minuto_partida):
+    minutos_chegada = hora_chegada * 60 + minuto_chegada
+    minutos_partida = hora_partida * 60 + minuto_partida
+
+    tempo_total_minutos = minutos_partida - minutos_chegada
+
+    if tempo_total_minutos <= 120:
+        return f"Preço: R$ {1:.2f}"
+    elif 180 <= tempo_total_minutos <= 240:
+        return f"Preço: R$ {1.4 * (tempo_total_minutos // 60):.2f}"
+    else:
+        return f"Preço: R$ {2 * (tempo_total_minutos // 60):.2f}"
