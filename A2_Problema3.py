106d2679df19356df4641efb9c4cd8f03b3a588f
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-10-09 16:58:42 -0300

    Create A2_Problema3.py

---
+++ A2_Problema3.py
@@ -0,0 +1,31 @@
+valor_hora = float(input("Digite o valor da hora de trabalho: ")
+qnt_horas = int(input("Digite a quantida de horas trabalhadas nos mês: ")
+
+salario = valor_hora * qnt_horas
+
+def pagamento (IR, imposto)
+  if salario <= 900
+    imposto = 0
+  elif salario <= 1500
+    imposto = 0,05
+  elif salario <= 2500
+    imposto = 0,10
+  else 
+    imposto = 0,20
+
+IR = salario * imposto
+return IR
+
+INSS = (salario * 0,10)
+FGTS = (salario * 0,11)
+
+desconto = IR + INSS
+
+print("Total de descontos: %.2f " % (desconto)
+      
+
+
+
+
+
+                
