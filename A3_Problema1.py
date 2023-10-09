1a399c668647abf47623e76f20bd5c2fe3271ed5
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-10-09 16:08:14 -0300

    Update A3_Problema1.py

--- A3_Problema1.py
+++ A3_Problema1.py
@@ -3,11 +3,10 @@ def progressoes(a1, r, n):
     an_pa = a1 + (n - 1) * r
     an_pg = a1 * r**(n - 1)
 
-    sm_pa = n * (a1 + an)/2
-    sm_pg = a1 * (r**n - 1)/ r - 1 
-
-if an_pa > an_pg and n = 9:
-   return sm_pa
-else: 
-   return sm_pg
+   if an_pa > an_pg 
+      resultado = n * (a1 + an_pa) / 2
+   else: 
+      resultado = a1 * (r ** n - 1) / (r - 1)
+      
+   return "Resultado %i " % (resultado)
     
