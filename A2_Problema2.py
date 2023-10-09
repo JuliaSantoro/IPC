237ed31922e3944930ffcdf6ddaa8c2a474d94d9
JuliaSantoro <134965436+JuliaSantoro@users.noreply.github.com>
2023-10-09 16:30:20 -0300

    Create A2_Problema2.py

---
+++ A2_Problema2.py
@@ -0,0 +1,28 @@
+print("É de Animação?")
+is_animation = int(input()) 
+
+if is_animation == 1:
+    print("É da Disney?")
+    is_disney = int(input())  
+    if is_disney == 1:
+        print("Resposta: Frozen")
+    else:
+        print("Resposta: Shrek")
+else:
+    print("É de Terror?")
+    is_horror = int(input()) 
+    if is_horror == 1:
+        print("É de Comédia?")
+        is_comedy = int(input()) 
+        if is_comedy == 1:
+            print("Resposta: Todo Mundo em Pânico")
+        else 
+            print("Resposta: Halloween")
+    else:
+          print("É de Drama?")
+          is_drama = int(input()) 
+          if is_drama == 1:
+              print("Resposta: Diário de Uma Paixão")
+          else:
+               print("Resposta: Como se Fosse a Primeira Vez")
+
